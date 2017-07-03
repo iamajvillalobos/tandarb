c84676f7140975f2daccff09c06816049c62cc31
Arman Jon Villalobos <me@armanjon.com>
2017-07-03 10:33:55 +0800

    Update test for Tandarb::Client

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,6 +2,6 @@ require "spec_helper"
 
 RSpec.describe Tandarb::Client do
   it "has proper base url" do
-    expect(described_class::BASE_URL).to eq "https://my.tanda.co/api/v2/"
+    expect(described_class::BASE_URL).to eq "https://my.tanda.co/api/v2"
   end
 end
